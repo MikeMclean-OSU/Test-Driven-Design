019074453bc47ecc2dccc45a388c8f9ea32c885d
MikeMclean-OSU <mcleamic@oregonstate.edu>
2025-05-08 16:59:08 -0400

    Wrote test2 to see if a password of length 8 is acceptd

--- test.py
+++ test.py
@@ -8,8 +8,11 @@ class TestPasswordFunc(unittest.TestCase):
 
     def test1(self):
         """Test to check if an empty password is valid"""
-
         self.assertFalse(check_pwd(""))
+
+    def test2(self):
+        """Test to check if passwords of length 8 pass"""
+        self.assertTrue(check_pwd("12345678"))
 
 
 if __name__ == '__main__':
