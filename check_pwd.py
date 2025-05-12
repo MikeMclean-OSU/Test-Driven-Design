a0b3c8c1443e62ad5477d7ed9d7fa8f111a32e4a
MikeMclean-OSU <mcleamic@oregonstate.edu>
2025-05-12 10:09:54 -0400

    Made check_pwd return False if all the characters in the password are lowercase, it passed

--- check_pwd.py
+++ check_pwd.py
@@ -6,5 +6,7 @@ def check_pwd(pwd):
         return False
     if not any(char.islower() for char in pwd):
         return False
+    if not any(char.isupper() for char in pwd):
+        return False
 
     return True
